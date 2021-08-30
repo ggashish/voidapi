28d0e11867105aa2a8139aa6c99d0fd19d89fb27
ggashish <ashishyadav11400@gmail.com>
2021-08-30 15:47:07 +0530

    name update

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import setup
 with open('README.md') as f:
     readme = f.read()
 
-setup(name='voidbots.py',
+setup(name='voidapi',
       author='Ashish Yadav',
       author_email='ashugodhai@gmail.com',
       version='1.0.0',
-      packages=['voidbots.py'],
+      packages=['voidapi'],
       license='MIT',
       description='A simple API wrapper for voidbots.net.',
       long_description=readme,
