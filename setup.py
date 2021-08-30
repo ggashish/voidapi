e66c5ca15857df0974af44c4c43b10420ad57cd4
ggashish <ashishyadav11400@gmail.com>
2021-08-30 21:02:48 +0530

    version update

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md') as f:
 setup(name='voidapi',
       author='Ashish Yadav',
       author_email='ashugodhai@gmail.com',
-      version='1.0.0',
+      version='1.0.1',
       packages=['voidapi'],
       license='MIT',
       description='A simple API wrapper for voidbots.net.',
